41457e91d29946ad14dafb75c68ce2d40d8ad613
Joaquín Pino <jpinoz@pm.me>
2021-10-17 21:32:13 -0500

    arreglos x2

--- Ciencia de la computación 2/matrix_multiplication.c
+++ Ciencia de la computación 2/matrix_multiplication.c
@@ -1,29 +1,17 @@
 #include <stdio.h>
-/*
-void mulMats(int mats[3][3][3])
-{
-    int *ptr1 = **mats;
-    int *ptr2 = **(mats + 1);
-    int *ptr3 = **(mats + 2);
-    int i, j, k;
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
-            *(ptr3 + (i * 3 + j)) = 0;
-            for (k = 0; k < 3; k++) {
-                *(ptr3 + (i * 3 + j)) += *(ptr1 + (i * 3 + k)) *
-                                         *(ptr2 + (k * 3 + j));
-            }
-        }
-    }
-}
-*/
 
 void mulMats(int mats[3][3][3])
 {
+    /* A * B = C */
     int (*A)[3] = *mats;
     int (*B)[3] = *(mats + 1);
     int (*C)[3] = *(mats + 2);
     int *p = *A, *q = *B, *r = *C;
+    /* limpiar C con 0s*/
+    for (; r < *C + 9; r++)
+        *r = 0;
+    r = *C;
+    /* multiplicar mats */
     while (p < *(A + 3)) {
         if (r == *(C + 1)) {
             r -= 3;
@@ -34,7 +22,7 @@ void mulMats(int mats[3][3][3])
             r = *(C + 1);
             C++;
         }
-        *r = *r + (*p) * (*q);
+        *r += *p * *q;
         q++;
         r++;
     }
@@ -54,14 +42,14 @@ int main(void)
 {
     int mats[3][3][3] = {
         {{1, 2, 3},
-         {3, 2, 1},
+         {3, 2, 1}, /* A */
          {1, 1, 2}},
         {{4, 5, 4},
-         {6, 5, 6},
+         {6, 5, 6}, /* B */
          {3, 5, 3}},
-        {{0, 0, 0},
-         {0, 0, 0},
-         {0, 0, 0}}
+        {{1, 1, 1},
+         {1, 1, 1}, /* C */
+         {1, 1, 1}}
     };
     mulMats(mats);
     printMat(*(mats+2));

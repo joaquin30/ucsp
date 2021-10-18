e678880ae0719cc8feda996d524ef03d8eeb47cd
Joaquín Pino <jpinoz@pm.me>
2021-10-17 20:16:43 -0500

    tarea de ccomp2

--- Ciencia de la computación 2/matrix_multiplication.c
+++ Ciencia de la computación 2/matrix_multiplication.c
@@ -5,22 +5,13 @@ void mulMats(int mats[3][3][3])
     int (*mat1)[3] = *mats;
     int (*mat2)[3] = *(mats + 1);
     int (*res)[3] = *(mats + 2);
-    int row, col, i, sum;
-    for (row = 0; row < 3; row++) {
-        for (col = 0; col < 3; col++) {
-            sum = 0;
-            for (i = 0; i < 3; i++)
-                sum += *(*(mat1 + row) + i) * *(*(mat2 + i) + col);
-            *(*(res + row) + col) = sum;
-        }
-    }
 }
 
 void printMat(int mat[3][3]) {
-    int i, j;
-    for (i = 0; i < 3; ++i) {
-        for (j = 0; j < 3; ++j)
-            printf("%d ", *(*(mat+i)+j));
+    int i, j, *ptr = *mat;
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++, ptr++)
+            printf("%d ", *ptr);
         puts("");
     }
 }
